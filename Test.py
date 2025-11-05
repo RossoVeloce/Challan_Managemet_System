70670c9bb3a4ad038856db9de34440f16d0677b8
RossoVeloce <apoorvaverma.hem@gmail.com>
2025-11-05 22:45:51 +0530

    Reformatted

--- Test.py
+++ Test.py
@@ -1,2 +1,11 @@
+# Admin Identification sys
+print("Hello hitmanshu")
 print("Hello, World!")
-print("Hello Hitmanshu")
+Admin = {"apoorva": "Apass", "himanshu": "Bpass"}
+U = input("Enter Username ")
+if U.lower() in Admin:
+  P = input("enter Password ")
+  if P == (Admin[U.lower()]):
+    print("permission given")
+  else:
+    print("permission denied")
