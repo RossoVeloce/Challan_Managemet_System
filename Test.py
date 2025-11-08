4d14008f612a820bcbc7c1d87ae842133f857c6a
WhatAUser-name <himchauhan1902@gmail.com>
2025-11-08 22:44:25 +0530

    Tablecheck + Error catching

--- Test.py
+++ Test.py
@@ -3,15 +3,23 @@ import mysql.connector as m
 # Admin Identification sys
 print("Hello hitmanshu")
 print("Hello, World!")
-Admin = {"apoorva": "Apass", "himanshu": "Bpass"}
+"""Admin = {"apoorva": "Apass", "himanshu": "Bpass"}
 U = input("Enter Username ")
 if U.lower() in Admin:
   P = input("enter Password ")
   if P == (Admin[U.lower()]):
     print("permission given")
   else:
-    print("permission denied")
-
-def challancheck():
-  Reg = input("Enter Vehicle Number")
-  db = m.connect(localhost="host",user="root",pwd="",database="_")
+    print("permission denied")"""
+def tablecheck():
+  #Reg = input("Enter Vehicle Number")
+  try:
+    db = m.connect(host="localhost",user="root",database="_")
+    print("Successfully connected :)")
+    cur = db.cursor()
+    cur.execute('show tables')
+    for i in cur:
+      print(i)
+  except:
+    print("Connection failed !!")
+tablecheck()
