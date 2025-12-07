17f871460ebeeb92ce7d63db6c153355d80dc5e5
WhatAUser-name <himchauhan1902@gmail.com>
2025-12-08 00:25:19 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -81,6 +81,14 @@ def add_chalan():
     print(f"New challan details are: {val}")
     bleh = ("INSERT INTO `challaninfo` VALUES (%s, %s, %s, %s, 'ACTIVE');")
     cur.execute(bleh, val)
+    val = val[0]
+    bleh = "SELECT COUNT(*) FROM challaninfo WHERE `Vehicle_ID`='{}' AND `Status`='ACTIVE';".format(val)
+    cur.execute(bleh)
+    for i in cur:
+      M = list(i)
+    M = M[0]
+    bleh = "UPDATE `challans` SET Pending_Challans= {},Past_Challans=Past_Challans WHERE `Vehicle_ID` = '{}';".format(M,val)
+    cur.execute(bleh)
     db.commit()
     cur.execute('select * from challaninfo')
     print("Challans:")
@@ -89,27 +97,22 @@ def add_chalan():
   except:
     print("Connection failed !!")
 
-def challanmodifier():
+def removechallan():
   try:
     db = m.connect(host="localhost",user="root",database="_")
     print("Successfully connected :)")
     cur = db.cursor()
-    M = int(input("Choose option: \n 1.Add Challan(NOT WORKING !) 2.Remove Challan "))
-    if M==2:
-      #M = int(input("Enter Vehicle ID: "))
-      M = int(input("Enter Challan ID: "))
-      Reg = "UPDATE challaninfo SET Status= 'INACTIVE' WHERE `Challan_ID` = '{}';".format(M)
-      cur.execute(Reg)
-      Reg = "SELECT `Vehicle_ID` FROM `challaninfo` WHERE `Challan_ID`='{}';".format(M)
-      cur.execute(Reg)
-      M = str(M)
-      for i in cur:
-        M = list(i)
-      M = M[0]
-      print (M,type(M))
-      Reg = "UPDATE `challans` SET Pending_Challans= Pending_Challans-1,Past_Challans=Past_Challans+1 WHERE `Vehicle_ID` = '{}' AND Pending_Challans>0;".format(M)
-    elif M==1:
-      print("Program in progress")
+    M = int(input("Enter Challan ID: "))
+    Reg = "UPDATE challaninfo SET Status= 'INACTIVE' WHERE `Challan_ID` = '{}';".format(M)
+    cur.execute(Reg)
+    Reg = "SELECT `Vehicle_ID` FROM `challaninfo` WHERE `Challan_ID`='{}';".format(M)
+    cur.execute(Reg)
+    M = str(M)
+    for i in cur:
+      M = list(i)
+    M = M[0]
+    print (M,type(M))
+    Reg = "UPDATE `challans` SET Pending_Challans= Pending_Challans-1,Past_Challans=Past_Challans+1 WHERE `Vehicle_ID` = '{}' AND Pending_Challans>0;".format(M)
     cur.execute(Reg)
     db.commit()
     print("Change Success")
@@ -120,7 +123,7 @@ def challanmodifier():
 
 #User Interface
 while True:
-  Opinion = int(input("What would you like to do ? (integer input) \n 1.Tablecheck \n 2.Check all challans \n 3.Check your challans \n 4.Modify challan \n 5.Add challan \n"))
+  Opinion = int(input("What would you like to do ? (integer input) \n 1.Tablecheck \n 2.Check all challans \n 3.Check your challans \n 4.Remove challan \n 5.Add challan \n"))
   if Opinion == 1:
     tablecheck()
     print() #formating
@@ -132,7 +135,7 @@ while True:
     specificcheck()
     print() #formatting
   elif Opinion == 4:
-    challanmodifier()
+    removechallan()
     print() #formatting
   elif Opinion == 5:
     add_chalan()
