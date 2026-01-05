599d5d1c2c6b7fd943eb1bc335e4f78360925c1f
RossoVeloce <apoorvaverma.hem@gmail.com>
2026-01-06 00:04:09 +0530

    Reformatted

--- Test.py
+++ Test.py
@@ -158,7 +158,7 @@ def add_vehicle():
 
 #User Interface
 while True:
-  Opinion = input("\nWhat would you like to do ? (integer input) \n 1.Tablecheck \n 2.Check all challans \n 3.Check your challans \n 4.Remove challan \n 5.Add challan \n 6.Add vehicle \n ")
+  Opinion = input("\nWhat would you like to do ? (integer input) \n 1.Tablecheck \n 2.Check all challans \n 3.Check your challans \n 4.Remove challan \n 5.Add challan \n 6.Add vehicle \n -->")
   if Opinion == '1' or Opinion.lower() == 'tablecheck':
     tablecheck()
     print() #formating
